ce046b9417a1f3b1a295b490c3a1eadc4cd39856
Nick Lozon <nicklozon@gmail.com>
2011-06-01 19:40:20 -0400

    Added lots of commenting Organized try/catch and exception statements onCommand only returns false if the command is not found, although it should always be found if the bukkit configuration is right? meh Removed some exceptions so errors are aborted more "softly" - no exceptions thrown unless it is ambiguous Removed the check for a valid region id format - thats more for creating new regions; an invalid region format will get thrown out when it finds it doesn't exist.

--- src/main/java/com/maxstnd/regionclaim/RegionClaim.java
+++ src/main/java/com/maxstnd/regionclaim/RegionClaim.java
@@ -12,7 +12,6 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.bukkit.WorldConfiguration;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
@@ -20,125 +19,143 @@ import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
-public class RegionClaim extends JavaPlugin
-{
-	private Logger m_log;
-	private PluginManager m_pm;
-	private WorldGuardPlugin m_wg;
-    
+public class RegionClaim extends JavaPlugin {
+    private Logger m_log;
+    private PluginManager m_pm;
+    private WorldGuardPlugin m_wg;
+
     public void onEnable() {
-    	m_log = Logger.getLogger("Minecraft");
-    	m_pm = this.getServer().getPluginManager();
-		
-    	m_log.info("RegionClaim enabled.");
+        // Get the logger and plugin manager
+        m_log = Logger.getLogger("Minecraft");
+        m_pm = this.getServer().getPluginManager();
+
+        m_log.info("RegionClaim enabled.");
     }
-    
+
     public void onDisable() {
-    	m_log.info("RegionClaim disabled.");
+        m_log.info("RegionClaim disabled.");
     }
-    
+
+    @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-    	if(m_wg == null) {
-	    	try {
-				m_wg = getWorldGuard();
-			} catch(Exception ex) {
-				sender.sendMessage(ChatColor.RED + ex.getMessage());
-	    		ex.printStackTrace();
-	    	}
-    	}
-		
-    	try {
-	    	if(cmd.getName().equalsIgnoreCase("regionclaim")) {
-	    		Player player;
-	    		player = m_wg.checkPlayer(sender);
-	    		LocalPlayer localPlayer = m_wg.wrapPlayer(player);
-	    		World world = player.getWorld();
-	    		String id = args[0];
-	    		
-	    		if(!ProtectedRegion.isValidId(id)) {
-	    			player.sendMessage(ChatColor.RED + "Invalid region ID specified!");
-	    			return false;
-	    		}
-	    		
-	    		if (id.equalsIgnoreCase("__global__")) {
-	    			player.sendMessage(ChatColor.RED + "A region cannot be named __global__");
-	    			return false;
-	            }
-	    		
-	    		RegionManager mgr = m_wg.getGlobalRegionManager().get(world);
-	            ProtectedRegion region = mgr.getRegion(id);
 
-	            if (region == null) {
-	            	player.sendMessage(ChatColor.RED + "Could not find a region by that ID.");
-	            	return false;
-	            }
-	            
-	            WorldConfiguration wcfg = m_wg.getGlobalConfiguration().get(player.getWorld());
-	            
-	            // Check whether the player has created too many regions 
-	            if (wcfg.maxRegionCountPerPlayer >= 0
-	                    && mgr.getRegionCountOfPlayer(localPlayer) >= wcfg.maxRegionCountPerPlayer) {
-	            	player.sendMessage(ChatColor.RED + "You own too many regions, delete one first to claim a new one.");
-	            	return false;
-	            }
-
-	            // Check whether the region has an owner
-	            if (region.getOwners().getPlayers().size() > 0) {
-	            	player.sendMessage(ChatColor.RED + "This region already has an owner - you can't claim it.");
-	            	return false;
-	            }
-	            
-	            ApplicableRegionSet regions = mgr.getApplicableRegions(region);
-	            
-	            // Check if this region overlaps any other region
-	            if (regions.size() == 0) {
-	                if (wcfg.claimOnlyInsideExistingRegions) {
-	                	player.sendMessage(ChatColor.RED + "You may only claim regions inside " +
-	                    		"existing regions that you or your group own.");
-	                	return false;
-	                }
-	            }
-
-	            if (region.volume() > wcfg.maxClaimVolume) {
-	                player.sendMessage(ChatColor.RED + "This region is to large to claim.");
-	                player.sendMessage(ChatColor.RED +
-	                        "Max. volume: " + wcfg.maxClaimVolume + ", your volume: " + region.volume());
-	                return false;
-	            }
-
-	            region.getOwners().addPlayer(player.getName());
-	            mgr.addRegion(region);
-	            
-	            try {
-	                mgr.save();
-	                sender.sendMessage(ChatColor.YELLOW + "Congratulations, you now own the region " + id + ".");
-	            } catch (IOException e) {
-	                throw new Exception("Failed to write regions file: "
-	                        + e.getMessage());
-	            }
-	    		
-	    		return true;
-	    	}
-	    } catch (Exception ex) {
-	    	ex.printStackTrace();
-			return false;
-		}
-    	
-    	// Command not found
-    	return false;
-    }
+        if (cmd.getName().equalsIgnoreCase("regionclaim")) {
+            try {
+                // Ensure we have WorldGuard else abort softly
+                if (!getWorldGuard(sender)) {
+                    return true;
+                }
+                
+                // Get player/world information
+                Player player = m_wg.checkPlayer(sender);
+                LocalPlayer localPlayer = m_wg.wrapPlayer(player);
+                World world = player.getWorld();
+                
+                // Check number of arguments passed
+                if(args.length != 1) {
+                    player.sendMessage(ChatColor.YELLOW + "/regionclaim <id>");
+                    return true;
+                }
+                String id = args[0];                
     
-    private WorldGuardPlugin getWorldGuard() throws Exception {
-    	Plugin worldGuard = m_pm.getPlugin("WorldGuard");
-    	
-        if (worldGuard == null) {
-            throw new Exception("WorldGuard does not appear to be installed.");
+                // Check that the region id specified is not __global__
+                if (id.equalsIgnoreCase("__global__")) {
+                    player.sendMessage(ChatColor.RED + "A region cannot be named __global__");
+                    return true;
+                }
+    
+                // Get the region manager and region
+                RegionManager mgr = m_wg.getGlobalRegionManager().get(world);
+                ProtectedRegion region = mgr.getRegion(id);
+    
+                // Check if the region id supplied is a valid one
+                if (region == null) {
+                    player.sendMessage(ChatColor.RED + "Could not find a region by that ID.");
+                    return true;
+                }
+    
+                // Get the world configuration
+                WorldConfiguration wcfg = m_wg.getGlobalConfiguration().get(player.getWorld());
+    
+                // Check whether the player has created too many regions
+                    // Note: Region count of 0 is "unlimited"
+                if (wcfg.maxRegionCountPerPlayer >= 0 && mgr.getRegionCountOfPlayer(localPlayer) >= wcfg.maxRegionCountPerPlayer) {
+                    player.sendMessage(ChatColor.RED + "You own too many regions, delete one first to claim a new one.");
+                    return true;
+                }
+    
+                // Check whether the region has an owner
+                if (region.getOwners().getPlayers().size() > 0) {
+                    player.sendMessage(ChatColor.RED + "This region already has an owner - you can't claim it.");
+                    return true;
+                }
+    
+                // Get the applicable regions for the region selection
+                ApplicableRegionSet regions = mgr.getApplicableRegions(region);
+    
+                // Check if this region overlaps any other region
+                if (regions.size() == 0) {
+                    if (wcfg.claimOnlyInsideExistingRegions) {
+                        player.sendMessage(ChatColor.RED + "You may only claim regions inside existing regions that you or your group own.");
+                        return true;
+                    }
+                }
+    
+                // Check if the region size is too large to claim
+                if (region.volume() > wcfg.maxClaimVolume) {
+                    player.sendMessage(ChatColor.RED + "This region is to large to claim.");
+                    player.sendMessage(ChatColor.RED + "Max. volume: " + wcfg.maxClaimVolume + ", your volume: " + region.volume());
+                    return true;
+                }
+    
+                // Add the player as the owner
+                region.getOwners().addPlayer(player.getName());
+                mgr.addRegion(region);
+    
+                // Attempt to save
+                try {
+                    mgr.save();
+                    sender.sendMessage(ChatColor.YELLOW + "Congratulations, you now own the region " + id + ".");
+                } catch (IOException e) {
+                    throw new Exception("Failed to write regions file: " + e.getMessage());
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            return true;
         }
-        
-        if (worldGuard instanceof WorldGuardPlugin) {
-            return (WorldGuardPlugin) worldGuard;
-        } else {
-            throw new Exception("WorldGuard detection failed (report error).");
+
+        // Command not found
+        return false;
+    }
+
+    /*
+     * Desc: Attempt to get the WorldGuard plugin
+     */
+    private boolean getWorldGuard(CommandSender sender) {
+        // Only load WorldGuard if it isn't already loaded
+        if (m_wg == null) {
+            try {
+                Plugin worldGuard = m_pm.getPlugin("WorldGuard");
+
+                // Check of WorldGuard is running, softly abort
+                if (worldGuard == null) {
+                    sender.sendMessage(ChatColor.RED + "WorldGuard does not appear to be installed.");
+                }
+
+                // Check if returned object is WorldGuard
+                if (worldGuard instanceof WorldGuardPlugin) {
+                    m_wg = (WorldGuardPlugin) worldGuard;
+                } else {
+                    throw new Exception("WorldGuard detection failed (report error).");
+                }
+            } catch (Exception ex) {
+                m_wg = null; // Reset WorldGuard
+                ex.printStackTrace();
+                return false;
+            }
         }
+        return true;
     }
 }
